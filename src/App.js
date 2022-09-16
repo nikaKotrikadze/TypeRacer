102edec5bf0598d8ecf3ff5623eaea9f5c28e765
nikak1414 <nikakotrikadze12@gmail.com>
2022-09-16 19:03:51 +0400

    correctWordsPlusAccuracyCommit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 var randomWords = require('random-words')
 
 export default function App (){
@@ -7,57 +7,96 @@ export default function App (){
     useEffect(()=>{
         setWords(generateWords())
     },[])
+
     function generateWords(){
-        return randomWords({min:10, max:20})
+        return randomWords({min:2, max:4})
     }
+    
     const [start, setStart]=useState(false)
     const [hideButton, setHideButton] = useState(true)
     const [changeColor, setChangeColor] = useState(false)
+    const [currInput, setCurrInput] = useState([])
+    const [currWordIndex, setCurrWordIndex] = useState(0)
+    const [correct, setCorrect]=useState(0)
+    const [incorrect, setIncorrect]=useState(0)
+    const [letter, setLetter]=useState('')
+    const textInput = useRef(null)
+
+    useEffect(()=>{
+        if(start){
+            textInput.current.focus();
+        }
+    },[start])
+
     const handleClick = () =>{
         setStart(prev=>!prev)
         setHideButton(prev=>!prev)
     }
 
-    const randomWordsArray = words
+    const handleKeyDown = ({keyCode}) =>{
+        if(keyCode === 32){
+            checkMatch()
+            setCurrInput('')
+            setCurrWordIndex(currWordIndex+1)
+        }
+        if(currWordIndex==words.length & keyCode===32){
+            setHideButton(prev=>!prev)
+            setStart(prev=>!prev)
+            setChangeColor(prev=>!prev)
+            setCurrWordIndex(0)
+            setCurrInput([])
+            setCorrect(0)
+            setIncorrect(0)
+            setWords(generateWords())
 
-    const inputWordsArray = []
-    let lastIndex;
-    const handleChange = ({target}) =>{
-        // inputWordsArray.push({index:target.value.split(' ')})
-        // lastIndex = (inputWordsArray[inputWordsArray.length-1].index)
-        // if(lastIndex.length==randomWordsArray.length){
-        //     console.log('truue as fuck my pos')
-        //     setChangeColor(prev=>!prev)
-    // }
-        console.log('changed')    
+        }
     }
-    
+    console.log(currInput, words[words.length-1])
+    function checkMatch(){
+        const wordToCompare = words[currWordIndex]
+        const doesItMatch = wordToCompare === currInput.trim()
+        // console.log({doesItMatch})
+        if(doesItMatch){
+            setCorrect(prevCorrect=>prevCorrect+1)
+        }else{
+            setIncorrect(prevIncorrect=>prevIncorrect+1)
+        }
+    }
 
   return (
     <div className='App'>
         <div className='content'>
             <div className='flex-words'>
                 {start===false?<div>click 'start' to begin</div>:
-                <div className='words' style={{display:start?'block':'none', color:changeColor?'green':''}}>
+                <div className='words' style={{display:start?'block':'none'}}>
                     {words.map((word,i)=>(
-                     
-                    <>
-                    <span key={i}>
-                        {word.split('').map((char,idx)=>(
-                            <span key={idx}>{char}</span>
-                        ))}
-                    </span>
-                    <span> </span>
-                    </>
-                    
+                        <>
+                            <span key={i}>
+                                {word.split('').map((char,idx)=>(
+                                    <span key={idx}>{char}</span>
+                                ))}
+                            </span>
+                            <span> </span>
+                        </>
                 ))}
                 </div>}
             </div>
             <div className='flex-input'>
-                <input placeholder='start typing here..' type='text' className='input' disabled={start?false:true} onChange={handleChange}/>
+                <input ref={textInput} placeholder='start typing here..' type='text' className='input' disabled={start?false:true} onKeyDown={handleKeyDown} value={currInput} onChange={(e)=>setCurrInput(e.target.value)}/>
             </div>
             <div className='flex-button'>
-                <button className='start-button' onClick={handleClick} style={{display:hideButton?'block':'none'}}>Start</button>
+                <button disabled={hideButton?false:true} className='start-button' onClick={handleClick}>Start</button>
+            </div>
+            <div className='stats'>
+                <div className='corr-words'>
+                    <p className='stats-title'>correct words: </p>
+                    <p className='stats-ans'>{correct}</p>
+                </div>
+                <div className='accuracy-words'>
+                    <p className='stats-title'>accuracy: </p>
+                    <p className='stats-ans'>{ Math.round((correct/(correct+incorrect))*100)? Math.round((correct/(correct+incorrect))*100):0} %</p>
+
+                </div>
             </div>
         </div>
     </div>
